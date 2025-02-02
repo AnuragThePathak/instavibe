fac75803a0b157f9ea669ddca3ad00e2688b8e7f
Anurag Pathak <anuragpathak911@gmail.com>
2025-02-02 21:36:30 +0530

    Implement login functionality with validation schema and user feedback

--- app/accounts/login/page.tsx
+++ app/accounts/login/page.tsx
@@ -4,74 +4,65 @@ import Loader from "@/components/loaders/spinner"
 import { Button } from "@/components/ui/button"
 import { FormField, FormItem, FormLabel, FormControl, FormMessage, Form } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-import { signUpSchema } from "@/utils/validation-schema"
+import { useAuthContext } from "@/context/authcontext"
+import { useToast } from "@/hooks/use-toast"
+import { useLoginUserMutation } from "@/react-query/mutations-queries"
+import { loginSchema } from "@/utils/validation-schema"
 import { zodResolver } from "@hookform/resolvers/zod"
 import Image from "next/image"
 import Link from "next/link"
+import { redirect } from "next/navigation"
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 
 export default function Page() {
-	const isLoading = false
+	const { toast } = useToast()
+	const { checkAuthUser, isPending: isUserLoading } = useAuthContext()
+	const { mutateAsync: loginUser, isPending: isLoggingIn } = useLoginUserMutation()
 
-	const form = useForm<z.infer<typeof signUpSchema>>({
-		resolver: zodResolver(signUpSchema),
+	const form = useForm<z.infer<typeof loginSchema>>({
+		resolver: zodResolver(loginSchema),
 		defaultValues: {
-			name: "",
-			username: "",
 			email: "",
 			password: ""
 		},
 	})
 
 	// 2. Define a submit handler.
-	function onSubmit(values: z.infer<typeof signUpSchema>) {
-		// Do something with the form values.
-		// ✅ This will be type-safe and validated.
-		console.log(values)
+	async function onSubmit(userData: z.infer<typeof loginSchema>) {
+		const session = await loginUser({
+			email: userData.email, password: userData.password
+		}) // also this try catch
+		if (!session) {
+			toast({
+				title: "Login Failed. Please try again",
+			})
+		}
+		
+		const isUserLoggedIn = await checkAuthUser()
+		if (isUserLoggedIn) {
+			redirect("/")
+		} else {
+			toast({
+				title: "Login Failed. Please try again",
+			})
+		}
 	}
 
 	return (
 		<div className="sm:w-420 flex-center flex-col">
-			<h1>Login Page</h1>
 			<Image src="/images/logo.svg"
 				alt="logo"
 				height="220"
 				width="220"
 				className="object-cover" />
-			<h1 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h1>
+			<h1 className="h3-bold md:h2-bold pt-5 sm:pt-12">Log in to your account</h1>
 			<p className="text-light-3 small-medium md:base-regular mt-2">
-				To use InstaVibe enter your details</p>
+			Welcome back! Please enter your details.</p>
 
 			<Form {...form}>
 				<form onSubmit={form.handleSubmit(onSubmit)}
 					className="flex flex-col gap-5 w-full mt-4">
-					<FormField
-						control={form.control}
-						name="name"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Name</FormLabel>
-								<FormControl>
-									<Input type="text" {...field} className="shad-input" />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<FormField
-						control={form.control}
-						name="username"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Username</FormLabel>
-								<FormControl>
-									<Input type="text" {...field} className="shad-input" />
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
 					<FormField
 						control={form.control}
 						name="email"
@@ -100,25 +91,25 @@ export default function Page() {
 					/>
 					<Button type="submit" className="shad-button_primary">
 						{
-							isLoading ?
+							 isLoggingIn || isUserLoading ?
 								<div className="flex-center gap-2">
 									<Loader /> Loading...
 								</div>
-								: "Sign Up"
+								: "Log In"
 						}
 					</Button>
 
 					<p className="text-small-regular text-light-2 text-center mt-2">
-				Already have an account?
-				<Link href="/accounts/login"
-					className="text-primary-500 text-small-semibold ml-1">
-					Log in
-				</Link>
-			</p>
+						Don&#39;t have an account?
+						<Link href="/accounts/signup"
+							className="text-primary-500 text-small-semibold ml-1">
+							Sign Up
+						</Link>
+					</p>
 				</form>
 			</Form>
 
-			
+
 		</div>
 	)
 }
--- utils/validation-schema.ts
+++ utils/validation-schema.ts
@@ -6,3 +6,8 @@ export const signUpSchema = z.object({
 	email: z.string().email(),
 	password: z.string().min(8)
 })
+
+export const loginSchema = z.object({
+	email: z.string().email(),
+	password: z.string().min(8)
+})
