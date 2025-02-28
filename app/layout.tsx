17be9b0f55dca16995b2e7104b8e73bfac08eb6a
Anurag Pathak <anuragpathak911@gmail.com>
2025-02-28 21:28:40 +0530

    Wrap AuthProvider in Suspense for improved loading handling and bump version to 0.1.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import { AuthProvider } from "@/context/authcontext"
 import QueryProviders from "@/lib/react-query-provider"
+import { Suspense } from "react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,9 +31,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <QueryProviders>
-          <AuthProvider>
-            {children}
-          </AuthProvider>
+          <Suspense>
+            <AuthProvider>
+              {children}
+            </AuthProvider>
+          </Suspense>
         </QueryProviders>
       </body>
     </html>
